3d5c4d86e747d5a62420b3645fa1bd3b54371c03
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2019-07-18 17:01:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file
+function findMatching(drivers, string) {
+  return drivers.filter(function(driver_obj) {
+    return driver_obj.toUpperCase() === string.toUpperCase();
+  })
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function(driver_obj) {
+    return driver_obj[0] === string[0];
+  })
+}
+
+function matchName(drivers,string) {
+  return drivers.filter(function(driver_obj) {
+    return driver_obj.name === string
+  });
+}
